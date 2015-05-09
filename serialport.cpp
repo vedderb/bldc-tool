e79a6c1579b3e130501b8a7f0a709e596df9d1a2
Rene Hopf <renehopf@mac.com>
2015-05-10 00:03:15 +0200

    add OS X support

--- serialport.cpp
+++ serialport.cpp
@@ -25,7 +25,9 @@
 #include <fcntl.h>   /* File control definitions */
 #include <errno.h>   /* Error number definitions */
 #include <termios.h> /* POSIX terminal control definitions */
+#ifndef __APPLE__
 #include <linux/serial.h>
+#endif
 #include <sys/ioctl.h>
 #include <iostream>
 
@@ -205,6 +207,7 @@ bool SerialPort::setBaudrate(int baudrate)
     case   57600: baud =   B57600; break;
     case  115200: baud =  B115200; break;
     case  230400: baud =  B230400; break;
+    #ifndef __APPLE__
     case  460800: baud =  B460800; break;
     case  500000: baud =  B500000; break;
     case  576000: baud =  B576000; break;
@@ -217,6 +220,7 @@ bool SerialPort::setBaudrate(int baudrate)
     case 3000000: baud = B3000000; break;
     case 3500000: baud = B3500000; break;
     case 4000000: baud = B4000000; break;
+    #endif
     default:      baud = 0; break;
     }
     
@@ -226,6 +230,7 @@ bool SerialPort::setBaudrate(int baudrate)
         return false;
     }
     
+    #ifndef __APPLE__
     serial_struct ser_info;
     
     if (0 == baud) {
@@ -272,6 +277,7 @@ bool SerialPort::setBaudrate(int baudrate)
         }
     }
     
+    #endif
     mSettings.baudrate = baudrate;
     return true;
 }
